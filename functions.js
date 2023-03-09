2cff20bf8e75576da94fd89e08f167d9af1d0022
Anas Asimi <anas2020cr7@gmail.com>
2023-03-09 15:16:18 +0100

    .

--- functions.js
+++ functions.js
@@ -2,17 +2,16 @@ import { exec } from 'node:child_process';
 
 
 function smsSender(target, message) {
+    let command = createShellCommand(target, message)
     exec(
-        createShellCommand(target, message),
+        command,
         { 'shell': 'powershell.exe' }
     )
 }
 
 
 function createShellCommand(target, message) {
-    let command = `adb shell service call isms 5 i32 0 s16 "com.google.android.apps.messaging" s16 "null" s16 "${target}" s16 "null" s16 "${message}"`
-    console.log(command);
-    return command
+    return `adb shell service call isms 5 i32 0 s16 "com.google.android.apps.messaging" s16 "null" s16 "${target}" s16 "null" s16 "'${message}'"`
 }
 
 
--- index.js
+++ index.js
@@ -4,23 +4,15 @@
 // adb shell input keyevent 66  
 
 
-
 // adb shell service call isms 5<interface function to use> i32 0<sim card to use> s16 "com.google.android.apps.messaging"<app pkg name to use> s16 "null" s16 "+212697748319"<target number> s16 "null" s16 "hello"<message text>
 // adb shell service call isms 5 i32 0 s16 "com.google.android.apps.messaging" s16 "null" s16 "+212697748319" s16 "null" s16 "hello"
-
 
 
 import {smsSender,createShellCommand} from './functions.js'
 
 
-
-
-
-
-
-
-
-for (let i = 1; i <= 10; i++) {
-    smsSender("+212697748319",`hello from nodeJs, message number ${i}, sent in ${(new Date).toDateString()}`)
-    // console.log(i);
+for (let i = 1; i <= 20; i++) {
+    let number = '+212697748319'
+    let message = `hello from nodeJs, sent in ${(new Date).toDateString()}`
+    smsSender(number,message)
 }
