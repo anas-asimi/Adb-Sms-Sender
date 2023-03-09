1d887d495d2e9a0b3dfb2306472425e51b47e3c2
Anas Asimi <anas2020cr7@gmail.com>
2023-03-09 15:17:08 +0100

    ...

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// adb shell am start -a android.intent.action.SENDTO -d sms:+972527300294 --es  sms_body "Test" --ez exit_on_sent true
-// adb shell input keyevent 22
-// adb shell input keyevent 22
-// adb shell input keyevent 66  
-
-
 // adb shell service call isms 5<interface function to use> i32 0<sim card to use> s16 "com.google.android.apps.messaging"<app pkg name to use> s16 "null" s16 "+212697748319"<target number> s16 "null" s16 "hello"<message text>
 // adb shell service call isms 5 i32 0 s16 "com.google.android.apps.messaging" s16 "null" s16 "+212697748319" s16 "null" s16 "hello"
 
